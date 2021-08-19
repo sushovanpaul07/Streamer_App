4d60d0934bbc151ee865470ec0725c920249076d
sushovanpaul07 <sushovanpaul07@gmail.com>
2021-08-19 18:27:32 +0530

    completed project

--- streamer_app_client/src/components/streams/StreamShow.js
+++ streamer_app_client/src/components/streams/StreamShow.js
@@ -1,10 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getStream } from "../../actions";
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
     this.props.getStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+  componentWillUnmount() {
+    this.flvPlayer.destroy();
+  }
+  buildPlayer() {
+    if (this.flvPlayer || !this.props.stream) {
+      return;
+    }
+    this.flvPlayer = flv.createPlayer({
+      type: "flv",
+      url: "http://localhost:8000/live/stream.flv",
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
   }
   render() {
     if (!this.props.stream) {
@@ -12,6 +35,7 @@ class StreamShow extends React.Component {
     }
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h2>{this.props.stream.title}</h2>
         <h5>{this.props.stream.description}</h5>
       </div>
